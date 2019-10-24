0e9b422691a8eb6f3208cdf2c3c8c9c976c8876f
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2019-10-24 20:51:31 +0200

    gg

--- git.sh
+++ git.sh
@@ -1,1 +1,1 @@
-rm -fr `find | grep .bak` && echo "var jsonCarData = '`cat data.json`'; " > js/data.js && git add . && git commit -m "gg" && git push
+echo "var jsonCarData = '`cat data.json`'; " > js/data.js && git add . && git commit -m "gg" && git push

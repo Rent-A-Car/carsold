305db6c5bd467021116f7974130c4a1716cc520b
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2019-10-24 20:59:18 +0200

    Thu Oct 24 20:59:18 CEST 2019

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ function setCarsTable()
 var x = 0;
 for (var i = 0; i < obj.length; i++){
 if (i==0){
-$("#Dcars").append("<option selected value='"+x.toString()+"'>"+obj[x].detiles.name+"</option>");
+$("#Dcars").append("<option selected value='"+x.toString()+"'>"+obj[x].details.name+"</option>");
 }else{
-$("#Dcars").append("<option value='"+x.toString()+"'>"+obj[x].detiles.name+"</option>");
+$("#Dcars").append("<option value='"+x.toString()+"'>"+obj[x].details.name+"</option>");
 };
 var scid = "t-"+ x.toString();
 $("#scars").append("<div class='tdata' id='"+scid+"'></div>");
 // цикл за фото
 $("#"+scid).append("<div class='galery'> <img src='"+obj[x].img[0]+"'></img> </div>");
-/////
+/////  <div class="vehicle-price">$ 37.40 <span class="info">rent per day</span></div>
 
-//$("#"+scid) append())
+$("#"+scid) append(" <div class='vehicle-price'>"+obj[x].details+"<span class='info'>"++"</span></div>");
 
 x = x+1;
 }
@@ -22,11 +22,11 @@ x = x+1;
 
   var keys = [];
   x = 0;
-  for (var k in obj[0].detiles)
+  for (var k in obj[0].details)
   {
     if (x > 2)
     {
-      keys.push(obj[0].detiles[k]);
+      keys.push(obj[0].details[k]);
     }
     x = x + 1
   };

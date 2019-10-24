de4337e30ea5c5a290fa539ff2e0faad8e29a573
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2019-10-24 15:42:47 +0200

    gg

--- js/script.js
+++ js/script.js
@@ -1,32 +1,40 @@
-function setCarsTable(div){
-var obj = JSON.parse(jsonCarData);
-var keys = [];
-var x = 0;
-for(var k in obj){
- if(x > 2){
- keys.push(k);
- }
- x = x+1
- };
-
-alert(keys);
-};
-function changeTC(value){
-alert(value);
-
-
-};
-
-$(document).ready(function()
+function setCarsTable(div)
 {
-setCarsTable("");
+  var obj = JSON.parse(jsonCarData);
 
 
-var carsc = $('.tdata').length;
-    $("#Dcars").change(function()
+
+  var keys = [];
+  var x = 0;
+  for (var k in obj[0].detiles)
+  {
+    if (x > 2)
     {
+      keys.push(obj[0].detiles[k]);
+    }
+    x = x + 1
+  };
+
+  $("#scars").append(keys);
+};
+
+function changeTC(value)
+{
+  alert(value);
+
+
+};
+
+$(document).ready(function ()
+{
+  setCarsTable("");
+
+
+  var carsc = $('.tdata').length;
+  $("#Dcars").change(function ()
+  {
     changeTC(this.value);
-    
-    });
+
+  });
 
 });

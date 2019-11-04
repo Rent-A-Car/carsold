1a4ea55a74a03716f7752a7ff456fd831ce0bb08
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2019-11-04 20:55:25 +0100

    Mon Nov  4 20:55:25 CET 2019

--- git.sh
+++ git.sh
@@ -1,1 +1,1 @@
-echo "var jsonCarData = '`cat data.json`'; " > js/data.js && git add . && git commit -m "` date `" && git push
+echo "var jsonCarData = '`cat data.json`'; " > js/data.js && echo "var LangObject= ' `cat lang.json ` '">> js/data.js && git add . && git commit -m "` date `" && git push

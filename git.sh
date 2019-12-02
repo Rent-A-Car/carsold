9ae5dc20ce5146ee1e9da6dc752676c7998ea852
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2019-12-02 16:40:32 +0100

    Mon Dec  2 16:40:32 CET 2019

--- git.sh
+++ git.sh
@@ -1,1 +1,1 @@
-echo "var jsonCarData = '`cat data.json`'; " > js/data.js && echo "var LangObject= ' `cat lang.json ` ';">> js/data.js && git add . && git commit -m "` date `" && git push origin beta
+echo "var jsonCarData = '`cat data.json`'; " > js/data.js && echo "var LangObject= ' `cat lang.json ` ';">> js/data.js && git add . && git commit -m "` date `" && git push origin master
